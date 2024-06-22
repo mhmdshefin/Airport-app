d38eeaa8a6ac8ab7891b7475d8b573eda65decd5
mhmd shefin <mshefin2005@gmail.com>
2024-06-22 22:35:31 +0530

    Airport app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from './components/Contact';
+import About from './components/About';
+import Service from './components/Service';
+import FAQ from './components/FAQ';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<HomePage/>}/>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/contact" element={<Contact/>}/>
+  <Route path="/service" element={<Service/>}/>
+  <Route path="/faq" element={<FAQ/>}/>
+</Routes>
+</BrowserRouter>
+
+
+
+
   );
 }
 
